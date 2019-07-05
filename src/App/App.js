15c99c954f96d8d3c51c89e339df53edb94be99c
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-07-05 15:42:16 -0500

    routing links work

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
+import NewHoard from '../components/NewHoard/NewHoard';
+import EditHoard from '../components/EditHoard/EditHoard';
+import SingleHoard from '../components/SingleHoard/SingleHoard';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import fbConnection from '../helpers/data/connection';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecher = props => (authed === false
+    ? (<Component {...props}/>)
+    : (<Redirect to ={ { pathname: '/home', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecher(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecher = props => (authed === true
+    ? (<Component {...props}/>)
+    : (<Redirect to ={ { pathname: '/auth', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecher(props)} />;
+};
+
 
 class App extends React.Component {
   state = {
@@ -30,16 +53,26 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      if (authed) {
-        return <Home />;
-      }
-      return <Auth />;
-    };
+
     return (
       <div className="App">
-        <MyNavbar authed={authed}/>
-        {loadComponent()}
+        <BrowserRouter>
+        <React.Fragment>
+          <MyNavbar authed={authed}/>
+          <div className='container'>
+            <div className="row">
+              <Switch>
+                <PublicRoute path='/auth' component={Auth} authed={authed}/>
+                <PrivateRoute path='/home' component={Home} authed={authed}/>
+                <PrivateRoute path='/new' component={NewHoard} authed={authed}/>
+                <PrivateRoute path='/edit/:id' component={EditHoard} authed={authed}/>
+                <PrivateRoute path='/hoard/:id' component={SingleHoard} authed={authed}/>
+                <Redirect from= "*" to= "/auth"/>
+              </Switch>
+            </div>
+          </div>
+        </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/NewHoard/NewHoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewHoard.scss';
+
+class NewHoard extends React.Component {
+  render() {
+    return (
+      <div className="NewHoard">
+        <h1>NewHoard</h1>
+      </div>
+    );
+  }
+}
+
+export default NewHoard;
