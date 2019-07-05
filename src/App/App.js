612d846c05404f70c89b2058926a23c21a578dd6
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-07-05 13:36:59 -0500

    login logout works

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import fbConnection from '../helpers/data/connection';
 
-function App() {
-  return (
-    <div className="App">
-      <button className='btn btn-danger'>hi</button>
-    </div>
-  );
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+    return (
+      <div className="App">
+        <MyNavbar authed={authed}/>
+        {loadComponent()}
+      </div>
+    );
+  }
 }
 
 export default App;
